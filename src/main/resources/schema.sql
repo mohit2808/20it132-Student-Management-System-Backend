f9507f9b0b032976058ba3edb70ec9c3b8d97e43
Mohitshah <20it132@charusat.edu.in>
2022-09-15 23:04:53 +0530

    Merge Branch Master

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,5 +8,3 @@ create table authorities (
                              authority varchar_ignorecase(50) not null,
                              constraint fk_authorities_users foreign key(username) references users(username));
 create unique index ix_auth_username on authorities (username,authority);
-
-//schemaSQLADDED
